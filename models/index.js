8dda45ae00f421f05b617a935921095a9fbf4aaa
grace363 <gracewambuigw170@gmail.com>
2025-07-14 19:51:54 +0300

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require('./User');
+const Video = require('./Video');
+const WatchHistory = require('./WatchHistory');
+const PaymentRequest = require('./PaymentRequest');
+const AppSettings = require('./AppSettings');
+const Admin = require('./Admin');
+const OwnerEarnings = require('./OwnerEarnings');
+const OwnerWithdrawal = require('./OwnerWithdrawal');
+
+module.exports = {
+  User,
+  Video,
+  WatchHistory,
+  PaymentRequest,
+  AppSettings,
+  Admin,
+  OwnerEarnings,
+  OwnerWithdrawal
+};
