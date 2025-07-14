cbb3ef9b9b70524275ae9c801d1a216e2f065679
grace363 <gracewambuigw170@gmail.com>
2025-07-14 19:55:43 +0300

    Update WatchHistory.js

--- models/WatchHistory.js
+++ models/WatchHistory.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const watchHistorySchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  videoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Video', required: true },
+  watchedDuration: { type: Number, required: true }, // in seconds
+  earnedAmount: { type: Number, default: 0 },
+  watchedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('WatchHistory', watchHistorySchema);
