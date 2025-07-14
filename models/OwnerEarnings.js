7c3c4a23ec5d5c0d5169f663552ca0caaf54bd9a
grace363 <gracewambuigw170@gmail.com>
2025-07-14 20:06:50 +0300

    Update OwnerEarnings.js

--- models/OwnerEarnings.js
+++ models/OwnerEarnings.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const ownerEarningsSchema = new mongoose.Schema({
+  totalEarnings: { type: Number, default: 0 },
+  todayEarnings: { type: Number, default: 0 },
+  lastReset: { type: Date, default: Date.now },
+  earningsHistory: [{
+    date: { type: Date, default: Date.now },
+    amount: Number,
+    source: String // 'user_activity', 'video_watch', etc.
+  }]
+});
+
+module.exports = mongoose.model('OwnerEarnings', ownerEarningsSchema);
