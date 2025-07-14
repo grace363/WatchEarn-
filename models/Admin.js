cb0048bdcae62766ba57d89c076afee243395a2b
grace363 <gracewambuigw170@gmail.com>
2025-07-14 20:05:37 +0300

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'super_admin'], default: 'admin' },
+  lastLogin: Date,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
